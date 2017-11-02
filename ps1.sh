6c55a6bc4f0aed46793c865d711dec02bc253836
Eduardo Rabelo <oieduardorabelo@gmail.com>
2017-11-02 17:20:52 +1300

    core: shell lint

--- ps1.sh
+++ ps1.sh
@@ -6,6 +6,7 @@ if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
   export GIT_PROMPT_THEME=Solarized
   export __GIT_PROMPT_DIR
   __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+  # shellcheck source=/usr/local/opt/bash-git-prompt/share/gitprompt.sh
   source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
 else
   #Prompt and prompt colors
