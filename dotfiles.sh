aa07b9c3fcb86522f3a25b3db93b3df5782e88ee
Eduardo Rabelo <oieduardorabelo@gmail.com>
2017-11-02 17:26:47 +1300

    core: node lts carbon

--- dotfiles.sh
+++ dotfiles.sh
@@ -23,7 +23,7 @@ fi
 
 # nvm
 if [[ ! "$CUSTOM_NODE_PATH" ]]; then
-  CUSTOM_NODE_PATH="$HOME/.nvm/versions/node/v8.0.0/bin"
+  CUSTOM_NODE_PATH="$HOME/.nvm/versions/node/v8.9.0/bin"
 fi
 
 export PATH=$PATH:$CUSTOM_NODE_PATH
