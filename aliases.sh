346b9dd1dea1b043fc0fa5d6bc4798aabc0a0957
Eduardo Rabelo <oieduardorabelo@gmail.com>
2018-04-10 23:21:15 +1200

    core: gc alias auto complete

--- aliases.sh
+++ aliases.sh
@@ -87,6 +87,7 @@ alias gm='git merge'
 # add git completion to aliases
 __git_complete g __git_main
 __git_complete gb _git_branch
+__git_complete gc _git_commit
 __git_complete gco _git_checkout
 __git_complete gm _git_merge
 __git_complete gp _git_push
