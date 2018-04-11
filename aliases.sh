7b08a80137140a080a9eb2ee7bd96bba9d802423
Eduardo Rabelo <oieduardorabelo@gmail.com>
2018-04-11 23:54:23 +1200

    core: RN aliases

--- aliases.sh
+++ aliases.sh
@@ -92,3 +92,19 @@ __git_complete gco _git_checkout
 __git_complete gm _git_merge
 __git_complete gp _git_push
 __git_complete gl _git_pull
+
+# react-native
+alias rn='react-native'
+alias rns='react-native start'
+alias rnlink='react-native link'
+
+alias rnand='react-native run-android'
+alias rnios='react-native run-ios'
+alias rnios4s='react-native run-ios --simulator "iPhone 4s"'
+alias rnios5='react-native run-ios --simulator "iPhone 5"'
+alias rnios5s='react-native run-ios --simulator "iPhone 5s"'
+alias rnios6='react-native run-ios --simulator "iPhone 6"'
+alias rnios6s='react-native run-ios --simulator "iPhone 6s"'
+
+alias rnland='react-native log-android'
+alias rnlios='react-native log-ios'
