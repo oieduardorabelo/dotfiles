921f6038a2497a1b2b761d366e085305f6a8435c
Eduardo Rabelo <oieduardorabelo@gmail.com>
2018-04-18 13:27:30 +1200

    node: using last lst/carbon

--- dotfiles.sh
+++ dotfiles.sh
@@ -30,7 +30,7 @@ fi
 
 # nvm / node
 if [[ ! "$CUSTOM_NODE_PATH" ]]; then
-  CUSTOM_NODE_PATH="$HOME/.nvm/versions/node/v8.10.0/bin"
+  CUSTOM_NODE_PATH="$HOME/.nvm/versions/node/v8.11.1/bin"
 fi
 
 nvm() {
