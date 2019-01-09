9ede6846aeabab636f2cd0be7e0d22ba08e813c7
Eduardo Rabelo <oieduardorabelo@gmail.com>
2019-01-10 10:06:15 +1100

    node: bump lts version

--- dotfiles.sh
+++ dotfiles.sh
@@ -17,7 +17,7 @@ if [[ ! "$EDITOR" ]]; then
 fi
 
 if [[ ! "$CUSTOM_NODE_PATH" ]]; then
-  CUSTOM_NODE_PATH="$HOME/.nvm/versions/node/v10.14.2/bin"
+  CUSTOM_NODE_PATH="$HOME/.nvm/versions/node/v10.15.0/bin"
 fi
 export NODE_PATH="$CUSTOM_NODE_PATH"
 
