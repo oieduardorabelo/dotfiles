5e4a59e71a95d7a1f1958291089d87cb58824429
Eduardo Rabelo <oieduardorabelo@gmail.com>
2019-03-19 22:44:24 +1100

    feat: git diff staged

--- aliases.sh
+++ aliases.sh
@@ -78,6 +78,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gds='git diff --staged'
 alias gc='git commit'
 alias gco='git checkout'
 alias gb='git branch'
