d0aaef053d3a5d3ec2d87495d6eb266469274d17
Eduardo Rabelo <oieduardorabelo@gmail.com>
2019-06-10 14:15:25 +1200

    node: latest lts

--- dotfiles.sh
+++ dotfiles.sh
@@ -18,7 +18,7 @@ if [[ ! "$EDITOR" ]]; then
 fi
 
 if [[ ! "$CUSTOM_NODE_PATH" ]]; then
-  CUSTOM_NODE_PATH="$HOME/.nvm/versions/node/v12.0.0/bin"
+  CUSTOM_NODE_PATH="$HOME/.nvm/versions/node/v10.16.0/bin"
 fi
 export NODE_PATH="$CUSTOM_NODE_PATH"
 
