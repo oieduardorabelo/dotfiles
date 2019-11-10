4743a6245ef92cbddc626eaca8f9ddb3f2e02f64
Eduardo Rabelo <oieduardorabelo@gmail.com>
2019-11-11 09:55:11 +1300

    docker

--- aliases.sh
+++ aliases.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# bat>cat
+## ########################################
+## bat > cat
+## ########################################
 alias cat='bat'
 
-# vim
+## ########################################
+## vim
+## ########################################
 alias svim='sudo vim'
 
-# yarn
+## ########################################
+## yarn
+## ########################################
 alias yr='yarn'
 alias yra='yarn add'
 alias yri='yarn install'
@@ -19,7 +25,9 @@ alias yrw='yarn workspace'
 alias yrg='yarn global'
 alias yrgl='yarn global list'
 
-# npm
+## ########################################
+## npm
+## ########################################
 alias npi='npm install'
 alias npl='npm ls --depth=0'
 alias npo='npm outdated'
@@ -27,10 +35,14 @@ alias npr='npm run'
 alias npt='npm init -y'
 alias npci='npm ci'
 
+## ########################################
 # ssh
+## ########################################
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | printf '\\n\\r\\033[2K  [ \\033[00;32mPublic key copied to pasteboard.\\033[0m ] id_rsa.pub\\n\\n'"
 
+## ########################################
 # misc
+## ########################################
 alias grep="grep --color=auto"
 alias less="less -r"
 alias dotfilesconfig='e ~/.dotfiles'
@@ -39,7 +51,9 @@ alias gitconfig='e ~/.gitconfig'
 alias hostsfile='e /etc/hosts'
 alias cafe!='echo wake up... && caffeinate'
 
+## ########################################
 # navigation
+## ########################################
 alias ..="cd ../"
 alias ...="cd ../../"
 alias ....="cd ../../../"
@@ -48,18 +62,24 @@ alias d="cd ~/Desktop"
 alias p="cd ~/Projects"
 alias dl="cd ~/Downloads"
 
+## ########################################
+## clear the console via terminal
+## same as cmd+k, macos only
+## ########################################
 k() {
-  # clear the console via terminal
-  # same as cmd+k, macos only
   printf '\33c\e[3J'
 }
 
+## ########################################
 # put mac to sleep via terminal
+## ########################################
 alias sleepnow="pmset sleepnow"
 
-# grc overides for ls
-#   made possible through contributions from generous benefactors like
-#   `brew install coreutils`
+## ########################################
+## grc overides for ls
+##   made possible through contributions from generous benefactors like
+##   `brew install coreutils`
+## ########################################
 if gls &>/dev/null; then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
@@ -72,7 +92,9 @@ else
   alias la="ls -A"
 fi
 
-# git
+## ########################################
+## git
+## ########################################
 alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
@@ -91,7 +113,9 @@ alias gs='git status -sb'
 alias gcb='git copy-branch-name'
 alias gm='git merge'
 
-# add git completion to aliases
+## ########################################
+## add git completion to aliases
+## ########################################
 __git_complete g __git_main
 __git_complete gb _git_branch
 __git_complete gc _git_commit
@@ -108,7 +132,9 @@ git_find_in_history() {
   git log --all --full-history -- "$1"
 }
 
-# react-native
+## ########################################
+## react-native
+## ########################################
 alias rn='react-native'
 alias rns='react-native start'
 alias rnlink='react-native link'
@@ -124,6 +150,21 @@ alias rnios6s='react-native run-ios --simulator "iPhone 6s"'
 alias rnland='react-native log-android'
 alias rnlios='react-native log-ios'
 
+## ########################################
+## docker
+## ########################################
+docker_start() {
+  docker-machine start default
+  docker-machine env default
+  eval $(docker-machine env default)
+}
+alias dr="docker"
+alias drc="docker-compose"
+alias drm="docker-machine"
+
+## ########################################
+## device simulators
+## ########################################
 open_android_emulator() {
   emulator @Pixel_API_28
 }
