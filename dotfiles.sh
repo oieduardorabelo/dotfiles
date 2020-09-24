82ede8d0d1493448144e10c36f2b86221a9420c9
Eduardo Rabelo <oieduardorabelo@gmail.com>
2020-09-24 14:11:09 +1200

    update lts/erbium

--- dotfiles.sh
+++ dotfiles.sh
@@ -25,7 +25,7 @@ if [[ ! "$EDITOR" ]]; then
 fi
 
 if [[ ! "$CUSTOM_NODE_PATH" ]]; then
-  CUSTOM_NODE_PATH="$HOME/.nvm/versions/node/v12.16.2/bin"
+  CUSTOM_NODE_PATH="$HOME/.nvm/versions/node/v12.18.4/bin"
 fi
 export NODE_PATH="$CUSTOM_NODE_PATH"
 
